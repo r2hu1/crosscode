9c318ad03580c4f21bf9b24f0a61dc5aa4318b00
r2hu1 <newasram86@gmail.com>
2026-09-07 22:57:58 +0530

    fix(keypress): store listener ref and clean up properly
    The data listener was never removed on cleanup, leaking handlers
    if onKeypress was called more than once. Now stores the reference
    and removes it in cleanupKeypress. Also adds 'h' key support and
    drops the unused emitKeypressEvents import.

--- packages/crosscode/src/keypress.ts
+++ packages/crosscode/src/keypress.ts
@@ -1,20 +1,30 @@
-import { emitKeypressEvents } from "node:readline";
+let dataListener: ((data: Buffer) => void) | null = null
 
 export function onKeypress(callback: (key: string) => void) {
-    emitKeypressEvents(process.stdin)
+    if (dataListener) {
+        process.stdin.removeListener("data", dataListener)
+    }
 
     if (process.stdin.isTTY) {
         process.stdin.setRawMode(true)
         process.stdin.resume()
     }
 
-    process.stdin.on("data", (data: Buffer) => {
+    dataListener = (data: Buffer) => {
         if (data.length === 1 && data[0] === 0x6c) callback("l")
+        else if (data.length === 1 && data[0] === 0x68) callback("h")
         else if (data.length === 1 && data[0] === 0x03) callback("ctrl-c")
-    })
+    }
+
+    process.stdin.on("data", dataListener)
 }
 
 export function cleanupKeypress() {
+    if (dataListener) {
+        process.stdin.removeListener("data", dataListener)
+        dataListener = null
+    }
+
     if (process.stdin.isTTY) {
         process.stdin.setRawMode(false)
         process.stdin.pause()
